64ac49ee8af7cf716e79cca7933e0a5911358909
Muhammet Akvardar <34101771+sreilus@users.noreply.github.com>
2019-10-09 11:48:13 +0300

    Web Api Oluşturulmaya Başlandı

---
+++ BankAppCoreWebApi/BankAppCoreWebApi/WebApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankAppCoreWebApi
+{
+	public class WebApiContext:DbContext
+	{
+		public DbSet<User> Users { get; set; }
+		public DbSet<Customer> Customers{ get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(@"Server=LAPTOP-0D83SARE\SQL2012KURDUM; Database=BankAppDb; Trusted_Connection=true;");
+		}
+	}
+}
