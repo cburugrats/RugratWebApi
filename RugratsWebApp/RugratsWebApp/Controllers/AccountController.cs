0c352c2a5b386ac6c1dcb489c78c0ccfa1f9effe
MSaitKrblk <m.sait.krblk@gmail.com>
2019-10-17 23:13:30 +0300

    Para Yatırma Çekme İşlemleri Eklendi

--- RugratsWebApp/RugratsWebApp/Controllers/AccountController.cs
+++ RugratsWebApp/RugratsWebApp/Controllers/AccountController.cs
@@ -15,8 +15,9 @@ namespace RugratsWebApp.Controllers
     [_SessionController]
     public class AccountController : Controller
 	{
-		// GET: Account
-		public async System.Threading.Tasks.Task<ActionResult> CreateAsync()
+        // GET: Account
+        [HttpGet]
+        public async System.Threading.Tasks.Task<ActionResult> CreateAsync()
 		{
             try
             {
@@ -66,7 +67,8 @@ namespace RugratsWebApp.Controllers
                 return RedirectToAction("List", "Account");
             }
 		}
-		public async System.Threading.Tasks.Task<ActionResult> List()
+        [HttpGet]
+        public async System.Threading.Tasks.Task<ActionResult> List()
 		{
             if (string.IsNullOrEmpty(User.Identity.Name))
             {
@@ -91,7 +93,6 @@ namespace RugratsWebApp.Controllers
 				  });
 				task.Wait();
 			}
-
 			return View(accounts);
 		}
         [HttpPost]
@@ -118,5 +119,191 @@ namespace RugratsWebApp.Controllers
 
             return RedirectToAction("List", "Account");
         }
-	}
+        [HttpGet]
+        public async System.Threading.Tasks.Task<ActionResult> ShortCuts()
+        {
+            if (string.IsNullOrEmpty(User.Identity.Name))
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            List<AccountModel> accounts = new List<AccountModel>();
+            using (var client = new HttpClient())
+            {
+                System.Net.ServicePointManager.ServerCertificateValidationCallback +=
+                (se, cert, chain, sslerror) =>
+                {
+                    return true;
+                };
+                var task = client.GetAsync("https://localhost:44329/api/account/" + User.Identity.Name)
+                  .ContinueWith((taskwithresponse) =>
+                  {
+                      var response = taskwithresponse.Result;
+                      var jsonString = response.Content.ReadAsStringAsync();
+                      jsonString.Wait();
+                      accounts = JsonConvert.DeserializeObject<List<AccountModel>>(jsonString.Result);
+
+                  });
+                task.Wait();
+            }
+            return View(accounts);
+        }
+        [HttpGet]
+	    public async System.Threading.Tasks.Task<ActionResult> MoneyWithdraw(string AccountNo)
+        {
+            if (string.IsNullOrEmpty(User.Identity.Name))
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            AccountModel account = new AccountModel();
+            using (var client = new HttpClient())
+            {
+                System.Net.ServicePointManager.ServerCertificateValidationCallback +=
+                (se, cert, chain, sslerror) =>
+                {
+                    return true;
+                };
+                var task = client.GetAsync("https://localhost:44329/api/account/getaccountbyid/" + AccountNo)
+                  .ContinueWith((taskwithresponse) =>
+                  {
+                      var response = taskwithresponse.Result;
+                      var jsonString = response.Content.ReadAsStringAsync();
+                      jsonString.Wait();
+                      account = JsonConvert.DeserializeObject<AccountModel>(jsonString.Result);
+
+                  });
+                task.Wait();
+            }
+            return View(account);
+        }
+        [HttpPost]
+        public async System.Threading.Tasks.Task<ActionResult> MoneyWithdrawAsync(withDrawMoneyModel collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                // Create a HttpClient
+                using (var client = new HttpClient())
+                {
+                    // Create post body object
+                    // Serialize C# object to Json Object
+                    var serializedProduct = JsonConvert.SerializeObject(collection);
+                    // Json object to System.Net.Http content type
+                    var content = new StringContent(serializedProduct, Encoding.UTF8, "application/json");
+                    // Post Request to the URI
+                    HttpResponseMessage result = await client.PostAsync("https://localhost:44329/api/account/withDrawMoney", content);
+                    // Check for result
+                    if (result.IsSuccessStatusCode)
+                    {
+                        result.EnsureSuccessStatusCode();
+                        string response = await result.Content.ReadAsStringAsync();
+                        if (response == "0")
+                        {
+                            return RedirectToAction("List", "Account");
+                        }
+                        else if (response == "1")
+                        {
+                            //Bilinmeyen Hata
+                            ViewBag.AccountResponse = "Succes";
+                            return RedirectToAction("List", "Account");
+                        }
+                        else
+                        {
+                            ViewBag.AccountResponse = "Unknown error occurred";
+                            return RedirectToAction("List", "Account");
+                        }
+                    }
+                    else
+                    {
+                        ViewBag.AccountResponse = "Unknown error occurred";
+                        return RedirectToAction("List", "Account");
+                    }
+                }
+            }
+            catch
+            {
+                ViewBag.AccountResponse = "Unknown error occurred";
+                return RedirectToAction("List", "Account");
+            }
+        }
+        [HttpGet]
+        public async System.Threading.Tasks.Task<ActionResult> DepositMoney(string AccountNo)
+        {
+            if (string.IsNullOrEmpty(User.Identity.Name))
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            AccountModel account = new AccountModel();
+            using (var client = new HttpClient())
+            {
+                System.Net.ServicePointManager.ServerCertificateValidationCallback +=
+                (se, cert, chain, sslerror) =>
+                {
+                    return true;
+                };
+                var task = client.GetAsync("https://localhost:44329/api/account/getaccountbyid/" + AccountNo)
+                  .ContinueWith((taskwithresponse) =>
+                  {
+                      var response = taskwithresponse.Result;
+                      var jsonString = response.Content.ReadAsStringAsync();
+                      jsonString.Wait();
+                      account = JsonConvert.DeserializeObject<AccountModel>(jsonString.Result);
+
+                  });
+                task.Wait();
+            }
+            return View(account);
+        }
+        [HttpPost]
+        public async System.Threading.Tasks.Task<ActionResult> DepositMoney(withDrawMoneyModel collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                // Create a HttpClient
+                using (var client = new HttpClient())
+                {
+
+                    // Create post body object
+                    // Serialize C# object to Json Object
+                    var serializedProduct = JsonConvert.SerializeObject(collection);
+                    // Json object to System.Net.Http content type
+                    var content = new StringContent(serializedProduct, Encoding.UTF8, "application/json");
+                    // Post Request to the URI
+                    HttpResponseMessage result = await client.PostAsync("https://localhost:44329/api/account/toDepositMoney", content);
+                    // Check for result
+                    if (result.IsSuccessStatusCode)
+                    {
+                        result.EnsureSuccessStatusCode();
+                        string response = await result.Content.ReadAsStringAsync();
+                        if (response == "0")
+                        {
+                            return RedirectToAction("List", "Account");
+                        }
+                        else if (response == "1")
+                        {
+                            //Bilinmeyen Hata
+                            ViewBag.AccountResponse = "Succes";
+                            return RedirectToAction("List", "Account");
+                        }
+                        else
+                        {
+                            ViewBag.AccountResponse = "Unknown error occurred";
+                            return RedirectToAction("List", "Account");
+                        }
+                    }
+                    else
+                    {
+                        ViewBag.AccountResponse = "Unknown error occurred";
+                        return RedirectToAction("List", "Account");
+                    }
+                }
+            }
+            catch
+            {
+                ViewBag.AccountResponse = "Unknown error occurred";
+                return RedirectToAction("List", "Account");
+            }
+        }
+    }
+
 }
